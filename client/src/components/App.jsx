cf83095761ca15c55d4f16b02885f32d0db038d1
Will McCulloch <willdmcculloch@gmail.com>
2021-08-14 14:31:28 -0700

    add seperate form component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import { Paper, CssBaseline, makeStyles, Box, Container, Button, TextField, Typo
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import SpacingDesign from './context/design.jsx';
 import Chart from './Chart.jsx';
+import Form from './Form.jsx';
 
 const App = (props) => {
   const [initial, setInitial] = useState(0);
@@ -20,13 +21,13 @@ const App = (props) => {
     if (e.target.name === 'income') {
       setIncome(Number(e.target.value));
     } else if (e.target.name === 'contribution') {
-      setContribution(Number(e.target.value)/100);
+      setContribution(Number(e.target.value) / 100);
     } else if (e.target.name === 'years') {
       setYears(Number(e.target.value));
     } else if (e.target.name === 'growth') {
-      setGrowth(Number(e.target.value)/100);
+      setGrowth(Number(e.target.value) / 100);
     } else if (e.target.name === 'variance') {
-      setVariance(Number(e.target.value)/100);
+      setVariance(Number(e.target.value) / 100);
     } else if (e.target.name === 'initial') {
       setInitial(Number(e.target.value));
     } else if (e.target.name === 'currentYear') {
@@ -41,73 +42,34 @@ const App = (props) => {
     let sum = tempCont;
     //iterate for input years - counting up from starting years
     for (let i = currentYear; i < (currentYear + years); i++) {
-        let obj = {};
-        let tempGrowth
-        if(data.length === 0) {
-            obj.name = i;
-            tempGrowth = sum * growth;
-            sum += tempGrowth;
-            obj.value = sum;
-            data.push(obj);
-        } else {
-            obj.name = i;
-            sum += tempCont;
-            tempGrowth = sum * growth;
-            sum += tempGrowth;
-            obj.value = sum;
-            data.push(obj);
-        }
+      let obj = {};
+      let tempGrowth
+      if (data.length === 0) {
+        obj.name = i;
+        tempGrowth = sum * growth;
+        sum += tempGrowth;
+        obj.value = sum;
+        data.push(obj);
+      } else {
+        obj.name = i;
+        sum += tempCont;
+        tempGrowth = sum * growth;
+        sum += tempGrowth;
+        obj.value = sum;
+        data.push(obj);
+      }
     }
     console.log(data);
     setData(data);
   }
   return (
-    <Container display='flex'>
+
+    <Container>
       <CssBaseline />
-      <Paper style={{
-        marginTop: '3vh',
-        flexBasis: '40%',
-        flexGrow: 0.5,
-        overflow: 'hidden',
-        borderRadius: '0',
-        width: '40vh'
-      }}>
-
-        {/* <Typography variant='h5' align='center' style={{
-          marginTop: '1vh',
-        }}>Retirement Calculator</Typography> */}
-
-        <Box display='flex' flexDirection='column' align='center' style={{
-          marginBottom: '1vh',
-          marginLeft: '1vh',
-          marginRight: '1vh',
-          fontSize: '1em'
-        }}>
-
-          <TextField id="form-input" type='number'  required name='initial' label="Initial Savings" onChange={handleChange} helperText='The starting savings value'/>
-          <TextField id="form-input" type='number'  required name='currentYear' label="Current Year" onChange={handleChange} helperText='The current year of the year you want to start saving'/>
-          <TextField id="form-input" type='number' required name='income' label="Salary" onChange={handleChange} helperText='Your annual salary'/>
-          <TextField id="form-input" type='number' required name='years' label="Years Until Retirement"  onChange={handleChange}
-          helperText='Roughly how many more years do you expect to work before you retire?'/>
-          <TextField id="form-input" type='number' required name='contribution' label="Monthly Contribution"  onChange={handleChange}
-          helperText='Percentage or you Monthy pay you wish to contribute.'/>
-          <TextField id="form-input" type='number' required name='growth' label="Estimated Growth Rate"  onChange={handleChange}
-          helperText='How much do you expect your savings to grow each year? (percentage)'/>
-          <TextField id="form-input" type='number' required name='variance' label="Growth Rate Variance"  onChange={handleChange}
-          helperText='Range of interest rates (above and below the rate set above) that you desire to see results for.'/>
-        </Box>
-
-        <Button display='flex' justifycontent='flex-end' color='primary' style={{
-          marginBottom: '2vh',
-          marginLeft: '2vh',
-          marginRight: '1vh',
-        }} onClick={handleSubmit}>Submit</Button>
-      </Paper>
+      <Form submit={handleSubmit} change={handleChange}/>
 
       <Chart data={data} />
     </Container>
-
-
   )
 }
 
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,50 @@
+import React, { useContext, useState } from 'react';
+import { Paper, CssBaseline, makeStyles, Box, Container, Button, TextField, Typography, FormControl, Input, InputLabel, FormHelperText } from '@material-ui/core';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import SpacingDesign from './context/design.jsx';
+import Chart from './Chart.jsx';
+
+const Form = (props) => {
+
+  return (
+    <Box>
+      <Paper style={{
+        marginTop: '3vh',
+        flexBasis: '40%',
+        flexGrow: 0.5,
+        overflow: 'hidden',
+        borderRadius: '0',
+        width: '40vh'
+      }}>
+        <Box display='flex' flexDirection='column' align='center' style={{
+          marginBottom: '1vh',
+          marginLeft: '1vh',
+          marginRight: '1vh',
+          fontSize: '1em'
+        }}>
+
+          <TextField id="form-input" type='number' required name='initial' label="Initial Savings" onChange={props.change} helperText='The starting savings value' />
+          <TextField id="form-input" type='number' required name='currentYear' label="Current Year" onChange={props.change} helperText='The current year of the year you want to start saving' />
+          <TextField id="form-input" type='number' required name='income' label="Salary" onChange={props.change} helperText='Your annual salary' />
+          <TextField id="form-input" type='number' required name='years' label="Years Until Retirement" onChange={props.change}
+            helperText='Roughly how many more years do you expect to work before you retire?' />
+          <TextField id="form-input" type='number' required name='contribution' label="Monthly Contribution" onChange={props.change}
+            helperText='Percentage or you Monthy pay you wish to contribute.' />
+          <TextField id="form-input" type='number' required name='growth' label="Estimated Growth Rate" onChange={props.change}
+            helperText='How much do you expect your savings to grow each year? (percentage)' />
+          <TextField id="form-input" type='number' required name='variance' label="Growth Rate Variance" onChange={props.change}
+            helperText='Range of interest rates (above and below the rate set above) that you desire to see results for.' />
+        </Box>
+
+        <Button display='flex' justifycontent='flex-end' color='primary' style={{
+          marginBottom: '2vh',
+          marginLeft: '2vh',
+          marginRight: '1vh',
+        }} onClick={props.submit}>Submit</Button>
+      </Paper>
+    </Box>
+
+  )
+}
+
+export default Form;
